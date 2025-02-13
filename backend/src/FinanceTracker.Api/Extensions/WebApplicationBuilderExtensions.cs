c82f44d7dd730e174632bdf76bb9b012b28dcaa2
sakibhossain323 <sakibhossain323@gmail.com>
2025-02-13 19:47:17 +0600

    refactor: add extension method for presentation layer services

---
+++ backend/src/FinanceTracker.Api/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Serilog;
+
+namespace FinanceTracker.Api.Extensions;
+
+public static class WebApplicationBuilderExtensions
+{
+    public static void AddPresentation(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Host.UseSerilog((context, configuration) =>
+            configuration.ReadFrom.Configuration(context.Configuration)
+        );
+    }
+}
