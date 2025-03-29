f315699920d8559e9254080f90724ff96acc1bbe
sakibhossain323 <sakibhossain323@gmail.com>
2025-03-30 03:00:47 +0600

    feat: complete login and storing session data

--- frontend/finance-tracker/src/app/page.tsx
+++ frontend/finance-tracker/src/app/page.tsx
@@ -2,6 +2,10 @@ import { auth } from "@/lib/auth";
 
 export default async function Home() {
     const session = await auth();
-    console.log(session);
-    return <>Home Page</>;
+    return (
+        <div>
+            <div>Home Page</div>
+            <div>Logged in as: {session?.user?.email}</div>
+        </div>
+    );
 }
--- frontend/finance-tracker/src/lib/auth.ts
+++ frontend/finance-tracker/src/lib/auth.ts
@@ -17,7 +17,25 @@ const attemptLogin = async (email: string, password: string) => {
     }
 };
 
-export const config = {
+const fetchUserInfo = async (token: string) => {
+    try {
+        const url = `${process.env.BACKEND_BASE_URL}/identity/manage/info`;
+        console.log("Fetching user info from", url);
+        const res = await fetch(url, {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        if (res?.ok) {
+            return await res.json();
+        }
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [
         Credentials({
             credentials: {
@@ -36,13 +54,38 @@ export const config = {
                     return null;
                 }
 
-                return await res.json();
+                const token = await res.json();
+                const user = await fetchUserInfo(token.accessToken);
+                return { ...user, token };
             },
         }),
     ],
+
     pages: {
         signIn: "/auth/login",
     },
-};
 
-export const { handlers, signIn, signOut, auth } = NextAuth(config);
+    session: {
+        strategy: "jwt",
+        maxAge: 3600,
+    },
+
+    callbacks: {
+        jwt: async ({ token, user }) => {
+            console.log("JWT:", token, user);
+            if (user) {
+                token.accessToken = user.token?.accessToken;
+                token.refreshToken = user.token?.refreshToken;
+            }
+            return token;
+        },
+        session: async ({ session, token }) => {
+            console.log("Session:", session, token);
+            if (token) {
+                session.accessToken = token.accessToken;
+                session.refreshToken = token.refreshToken;
+            }
+            return session;
+        },
+    },
+});
