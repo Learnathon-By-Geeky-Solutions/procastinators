2f75973cbe100d98e43267480852228574bf0b03
rafio1020 <rafio.work.2002@gmail.com>
2025-04-07 03:09:21 +0600

    test: add update personal transaction command validator and valid command

---
+++ backend/tests/FinanceTracker.Application.Tests/PersonalTransactions/Commands/UpdatePersonalTransaction/UpdatePersonalTransactionCommandValidatorTests.cs
@@ -0,0 +1,35 @@
+﻿using FinanceTracker.Domain.Entities;
+using FluentValidation.TestHelper;
+using Xunit;
+
+namespace FinanceTracker.Application.PersonalTransactions.Commands.UpdatePersonalTransaction.Tests;
+
+public class UpdatePersonalTransactionCommandValidatorTests
+{
+    [Theory()]
+    [InlineData("Income")]
+    [InlineData("Expense")]
+    public void Validator_ForValidCommand_ShouldNotHaveValidationErrors(string transactionType)
+    {
+        // Arrange
+
+        var command = new UpdatePersonalTransactionCommand()
+        {
+            TransactionType = transactionType,
+            Amount = 100,
+            Timestamp = DateTime.Parse("2025-04-07"),
+            CategoryId = 23,
+            WalletId = 11
+        };
+
+        var validator = new UpdatePersonalTransactionCommandValidator();
+
+        // Act
+
+        var results = validator.TestValidate(command);
+
+        // Assert
+
+        results.ShouldNotHaveAnyValidationErrors();
+    }
+}
