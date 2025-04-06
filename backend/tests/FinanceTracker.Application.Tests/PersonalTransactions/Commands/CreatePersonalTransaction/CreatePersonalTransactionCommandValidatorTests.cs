ce7c69df0c395c386d91cd43111a44b4871a6570
rafio1020 <rafio.work.2002@gmail.com>
2025-04-06 22:07:03 +0600

    test: add create personal treansaction command validator for valid commands

---
+++ backend/tests/FinanceTracker.Application.Tests/PersonalTransactions/Commands/CreatePersonalTransaction/CreatePersonalTransactionCommandValidatorTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation.TestHelper;
+using Microsoft.AspNetCore.Http;
+using Xunit;
+
+namespace FinanceTracker.Application.PersonalTransactions.Commands.CreatePersonalTransaction.Tests;
+
+public class CreatePersonalTransactionCommandValidatorTests
+{
+    [Fact()]
+    public void Validator_ForValidCommand_ShouldNotHaveValidationErrors()
+    {
+        // Arrange
+
+        var command = new CreatePersonalTransactionCommand()
+        {
+            TransactionType = "Expense",
+            Amount = 45,
+            WalletId = 34,
+            CategoryId = 24
+        };
+
+        var validator = new CreatePersonalTransactionCommandValidator();
+
+        // Act
+
+        var results = validator.TestValidate(command);
+
+        // Assert
+
+        results.ShouldNotHaveAnyValidationErrors();
+    }
+
+}
