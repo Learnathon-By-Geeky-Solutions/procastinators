abafb357d7ea7c1c05849fea35326dd55ff0968b
rafio1020 <rafio.work.2002@gmail.com>
2025-04-09 00:39:10 +0600

    test: add create map for wallet to wallet dto in wallet profile which maps correctly

---
+++ backend/tests/FinanceTracker.Application.Tests/Wallets/Dtos/WalletProfileTests.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using FinanceTracker.Application.Categories.Dtos;
+using FinanceTracker.Domain.Entities;
+using FluentAssertions;
+using Xunit;
+
+namespace FinanceTracker.Application.Wallets.Dtos.Tests;
+
+public class WalletProfileTests
+{
+    private IMapper _mapper;
+
+    public WalletProfileTests()
+    {
+        var configuration = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<WalletProfile>();
+        });
+
+        _mapper = configuration.CreateMapper();
+    }
+
+    [Fact()]
+    public void CreateMap_ForWalletToWalletDto_MapsCorrectly()
+    {
+        // Arrange
+
+        var wallet = new Wallet()
+        {
+            Id = 1,
+            Type = "Bank",
+            Name = "name",
+            Currency = "BDT",
+            Balance = 4000
+        };
+
+        // Act
+
+        var walletDto = _mapper.Map<WalletDto>(wallet);
+
+        // Assert
+
+        walletDto.Should().NotBeNull();
+        walletDto.Id.Should().Be(wallet.Id);
+        walletDto.Type.Should().Be(wallet.Type);
+        walletDto.Name.Should().Be(wallet.Name);
+        walletDto.Currency.Should().Be(wallet.Currency);
+        walletDto.Balance.Should().Be(wallet.Balance);
+    }
+}
