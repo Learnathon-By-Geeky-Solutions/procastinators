da71ff266247617374f16d5375bdcd4e6198f89c
rafio1020 <rafio.work.2002@gmail.com>
2025-04-08 23:52:09 +0600

    test: add create map for create category command to category which maps correctly

--- backend/tests/FinanceTracker.Application.Tests/Categories/Dtos/CategoryProfileTests.cs
+++ backend/tests/FinanceTracker.Application.Tests/Categories/Dtos/CategoryProfileTests.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using FinanceTracker.Application.Categories.Commands.CreateCategory;
 using FinanceTracker.Domain.Entities;
 using FluentAssertions;
 using Xunit;
@@ -36,5 +37,36 @@ public class CategoryProfileTests
         categoryDto.Id.Should().Be(category.Id);
         categoryDto.Title.Should().Be(category.Title);
         categoryDto.DefaultTransactionType.Should().Be(category.DefaultTransactionType);
+    }
+
+    [Theory()]
+    [InlineData("Income")]
+    [InlineData("Expense")]
+    public void CreateMap_ForCreateCategoryCommandToCategory_MapsCorrectly(string defaultTransactionTypes)
+    {
+        // Arrange
+
+        var configuration = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<CategoryProfile>();
+        });
+
+        var mapper = configuration.CreateMapper();
+
+        var command = new CreateCategoryCommand()
+        {
+            Title = "234",
+            DefaultTransactionType = defaultTransactionTypes
+        };
+
+        // Act
+
+        var category = mapper.Map<Category>(command);
+
+        // Assert
+
+        category.Should().NotBeNull();
+        category.Title.Should().Be(category.Title);
+        category.DefaultTransactionType.Should().Be(category.DefaultTransactionType);
     }
 }
