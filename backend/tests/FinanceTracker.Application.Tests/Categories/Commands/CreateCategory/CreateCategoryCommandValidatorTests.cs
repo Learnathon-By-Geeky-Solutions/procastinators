76fb2cd4c20b3250e594fa49176b675f00c7710b
rafio1020 <rafio.work.2002@gmail.com>
2025-04-08 21:00:43 +0600

    test: add create category command valaidator test with valid and invalid commands

---
+++ backend/tests/FinanceTracker.Application.Tests/Categories/Commands/CreateCategory/CreateCategoryCommandValidatorTests.cs
@@ -0,0 +1,54 @@
+﻿using FluentValidation.TestHelper;
+using Xunit;
+
+namespace FinanceTracker.Application.Categories.Commands.CreateCategory.Tests;
+
+public class CreateCategoryCommandValidatorTests
+{
+    [Theory()]
+    [InlineData("Income")]
+    [InlineData("Expense")]
+    public void CreateCategoryCommandValidator_ForValidCommand_ShouldNotHaveValidationErrors(string transactionType)
+    {
+        // Arrange
+
+        var command = new CreateCategoryCommand()
+        {
+            Title = "test",
+            DefaultTransactionType = transactionType
+        };
+
+        var validator = new CreateCategoryCommandValidator();
+
+        // Act
+
+        var results = validator.TestValidate(command);
+
+        // Assert
+
+        results.ShouldNotHaveAnyValidationErrors();
+    }
+
+    [Fact()]
+    public void CreateCategoryCommandValidator_FoInvalidCommand_ShouldNotHaveValidationErrors()
+    {
+        // Arrange
+
+        var command = new CreateCategoryCommand()
+        {
+            Title = "",
+            DefaultTransactionType = "test"
+        };
+
+        var validator = new CreateCategoryCommandValidator();
+
+        // Act
+
+        var results = validator.TestValidate(command);
+
+        // Assert
+
+        results.ShouldHaveValidationErrorFor(c => c.Title);
+        results.ShouldHaveValidationErrorFor(c => c.DefaultTransactionType);
+    }
+}
