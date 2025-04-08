d4491afdda17e97e722705a5871571686c064ff2
rafio1020 <rafio.work.2002@gmail.com>
2025-04-08 23:44:19 +0600

    test: add create map for category to category dto in category profile which maps correctly

---
+++ backend/tests/FinanceTracker.Application.Tests/Categories/Dtos/CategoryProfileTests.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using FinanceTracker.Domain.Entities;
+using FluentAssertions;
+using Xunit;
+
+namespace FinanceTracker.Application.Categories.Dtos.Tests;
+
+public class CategoryProfileTests
+{
+    [Fact()]
+    public void CreateMap_ForCategoryToCategoryDto_MapsCorrectly()
+    {
+        // Arrange
+
+        var configuration = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<CategoryProfile>();
+        });
+
+        var mapper = configuration.CreateMapper();
+
+        var category = new Category()
+        {
+            Id = 1,
+            Title = "test",
+            DefaultTransactionType = "Income",
+        };
+
+        // Act
+
+        var categoryDto = mapper.Map<CategoryDto>(category);
+
+        // Assert
+
+        categoryDto.Should().NotBeNull();
+        categoryDto.Id.Should().Be(category.Id);
+        categoryDto.Title.Should().Be(category.Title);
+        categoryDto.DefaultTransactionType.Should().Be(category.DefaultTransactionType);
+    }
+}
