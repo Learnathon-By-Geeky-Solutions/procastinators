3f3d512d6b7213219830050287b855f5cdc10041
rafio1020 <rafio.work.2002@gmail.com>
2025-04-09 15:56:35 +0600

    test: add create map for create personal transaction command to personal transacction in personal transaction profile test which maps correctly

--- backend/tests/FinanceTracker.Application.Tests/PersonalTransactions/Dtos/PersonalTransactionProfileTests.cs
+++ backend/tests/FinanceTracker.Application.Tests/PersonalTransactions/Dtos/PersonalTransactionProfileTests.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using FinanceTracker.Application.PersonalTransactions.Commands.CreatePersonalTransaction;
 using FinanceTracker.Application.Wallets.Dtos;
 using FinanceTracker.Domain.Entities;
 using FluentAssertions;
@@ -50,6 +51,36 @@ public class PersonalTransactionProfileTests
         personalTransactionDto.Note.Should().Be(personalTransaction.Note);
         personalTransactionDto.WalletId.Should().Be(personalTransaction.WalletId);
         personalTransactionDto.CategoryId.Should().Be(personalTransaction.CategoryId);
+    }
+
+    [Fact()]
+    public void CreateMap_ForCreatePersonalTransactionCommandToPersonalTransaction_MapsCorrectly()
+    {
+        // Arrange
+
+        var command = new CreatePersonalTransactionCommand()
+        {
+            TransactionType = "Income",
+            Amount = 500,
+            Timestamp = DateTime.Parse("2022-12-05 08:30:00"), 
+            Note = "Its me the tester haha",
+            WalletId = 12,
+            CategoryId = 1
+        };
+
+        // Act
+
+        var personalTransaction = _mapper.Map<PersonalTransaction>(command);
+
+        // Assert
+
+        personalTransaction.Should().NotBeNull();
+        personalTransaction.TransactionType.Should().Be(command.TransactionType);
+        personalTransaction.Amount.Should().Be(command.Amount);
+        personalTransaction.Timestamp.Should().Be(command.Timestamp);
+        personalTransaction.Note.Should().Be(command.Note);
+        personalTransaction.WalletId.Should().Be(command.WalletId);
+        personalTransaction.CategoryId.Should().Be(command.CategoryId);
     }
 }
 
