2270ccf4b382e79f366d230d478fe5e4ff926a82
Ashik150 <sskhan4409@gmail.com>
2025-04-12 03:23:09 +0600

    feat: getAllLoanRequests Query done

---
+++ backend/src/FinanceTracker.Application/LoanRequests/Queries/GetAllLoanRequests/GetAllLoanRequestQuery.cs
@@ -0,0 +1,8 @@
+﻿using FinanceTracker.Application.LoanRequests.Dtos;
+using MediatR;
+
+namespace FinanceTracker.Application.LoanRequests.Queries.GetAllLoanRequests;
+
+public class GetAllLoanRequestQuery : IRequest<IEnumerable<LoanRequestDto>>
+{
+}
---
+++ backend/src/FinanceTracker.Application/LoanRequests/Queries/GetAllLoanRequests/GetAllLoanRequestQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using FinanceTracker.Application.Categories.Queries.GetCategoryById;
+using FinanceTracker.Application.LoanRequests.Dtos;
+using FinanceTracker.Domain.Repositories;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace FinanceTracker.Application.LoanRequests.Queries.GetAllLoanRequests;
+
+public class GetAllLoanRequestQueryHandler(ILogger<GetAllLoanRequestQueryHandler> logger,
+    ILoanRequestRepository repo,
+    IMapper mapper) : IRequestHandler<GetAllLoanRequestQuery, IEnumerable<LoanRequestDto>>
+{
+    public async Task<IEnumerable<LoanRequestDto>> Handle(GetAllLoanRequestQuery request, CancellationToken cancellationToken)
+    {
+        var list = await repo.GetAllAsync();
+        logger.LogInformation("list: {@r}",list);
+        return mapper.Map<IEnumerable<LoanRequestDto>>(list);
+    }
+}
