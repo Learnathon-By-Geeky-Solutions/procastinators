f028479441730f3021a262a773fc6a429849f0eb
Ashik150 <sskhan4409@gmail.com>
2025-04-12 00:40:52 +0600

    feat: add Dtos for Loan Request

---
+++ backend/src/FinanceTracker.Application/LoanRequests/Dtos/LoanRequestDto.cs
@@ -0,0 +1,14 @@
+﻿
+namespace FinanceTracker.Application.LoanRequests.Dtos;
+
+public class LoanRequestDto
+{
+    public int Id { get; set; }
+    public decimal Amount { get; set; }
+    public string? Note { get; set; }
+    public DateTime DueDate { get; set; }
+
+    public string BorrowerId { get; set; } = default!;
+    public string LenderId { get; set; } = default!;
+    public bool IsApproved { get; set; }
+}
---
+++ backend/src/FinanceTracker.Application/LoanRequests/Dtos/LoanRequestProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using FinanceTracker.Application.LoanRequests.Commands;
+using FinanceTracker.Domain.Entities;
+namespace FinanceTracker.Application.LoanRequests.Dtos;
+
+public class LoanRequestProfile : Profile
+{
+    public LoanRequestProfile()
+    {
+        CreateMap<CreateLoanRequestCommand, LoanRequest>();
+        CreateMap<LoanRequest,LoanRequestDto>();
+    }
+}
