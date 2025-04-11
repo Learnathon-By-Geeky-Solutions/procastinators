365d35d6f668f88f3c53a27b7b55a355e40f787b
Ashik150 <sskhan4409@gmail.com>
2025-04-11 22:34:11 +0600

    feat: add all Loan related IRepositoty interfaces

---
+++ backend/src/FinanceTracker.Domain/Repositories/IInstallmentRepository.cs
@@ -0,0 +1,12 @@
+﻿
+using FinanceTracker.Domain.Entities;
+
+namespace FinanceTracker.Domain.Repositories;
+
+public interface IInstallmentRepository
+{
+    Task<int> CreateAsync(Installment installment);
+    Task<Installment?> GetByIdAsync(int id);
+    Task<IEnumerable<Installment>> GetAllAsync();
+    Task<int> SaveChangesAsync();
+}
---
+++ backend/src/FinanceTracker.Domain/Repositories/ILoanRepository.cs
@@ -0,0 +1,12 @@
+﻿
+using FinanceTracker.Domain.Entities;
+
+namespace FinanceTracker.Domain.Repositories;
+
+public interface ILoanRepository
+{
+    Task<int> CreateAsync(Loan loan);
+    Task<Loan?> GetByIdAsync(int id);
+    Task<IEnumerable<Loan>> GetAllAsync();
+    Task<int> SaveChangesAsync();
+}
