faa63ef127da45b7f5812de6b2daa4502d960125
rafio1020 <rafio.work.2002@gmail.com>
2025-04-12 17:07:37 +0600

    test: add method for null user which returns forbidden exception in create category handler

--- backend/tests/FinanceTracker.Application.Tests/Categories/Commands/CreateCategory/CreateCategoryCommandHandlerTests.cs
+++ backend/tests/FinanceTracker.Application.Tests/Categories/Commands/CreateCategory/CreateCategoryCommandHandlerTests.cs
@@ -3,8 +3,10 @@ using Castle.Core.Logging;
 using FinanceTracker.Application.Users;
 using FinanceTracker.Application.Wallets.Commands.CreateWallet;
 using FinanceTracker.Domain.Entities;
+using FinanceTracker.Domain.Exceptions;
 using FinanceTracker.Domain.Repositories;
 using FluentAssertions;
+using Microsoft.ApplicationInsights.Extensibility.Implementation;
 using Microsoft.Extensions.Logging;
 using Moq;
 using System.Threading.Tasks;
@@ -50,6 +52,41 @@ public class CreateCategoryCommandHandlerTests
         result.Should().Be(1);
         category.UserId.Should().Be("test");
         categoryRepositoryMock.Verify(r => r.Create(category), Times.Once);
+
+    }
+
+    [Fact()]
+    public async Task Handle_ForUserNull_ReturnsForbiddenException()
+    {
+        // Arrange
+
+        var loggerMock = new Mock<ILogger<CreateCategoryCommandHandler>>();
+
+        var userContextMock = new Mock<IUserContext>();
+        userContextMock
+            .Setup(u => u.GetUser())
+            .Returns((UserDto?)null);
+
+        var mapperMock = new Mock<IMapper>();
+        var command = new CreateCategoryCommand();
+        var category = new Category();
+        mapperMock
+            .Setup(m => m.Map<CreateCategoryCommand, Category>(command))
+            .Returns(category);
+
+        var categoryRepositoryMock = new Mock<ICategoryRepository>();
+        categoryRepositoryMock
+            .Setup(repo => repo.Create(It.IsAny<Category>()))
+            .ReturnsAsync(1);
+
+        var commandHandler = new CreateCategoryCommandHandler(loggerMock.Object, userContextMock.Object, mapperMock.Object, categoryRepositoryMock.Object);
+
+        // Act
+
+        await FluentActions
+        .Invoking(() => commandHandler.Handle(command, CancellationToken.None))
+        .Should()
+        .ThrowAsync<ForbiddenException>();
 
     }
 }
