3c7f348b820155b47f47e9ce21f8ff9704ecb3e4
rafio1020 <rafio.work.2002@gmail.com>
2025-04-17 11:41:09 +0600

    test: add condition for user null in create wallet command handler

--- backend/tests/FinanceTracker.Application.Tests/Wallets/Commands/CreateWallet/CreateWalletCommandHandlerTests.cs
+++ backend/tests/FinanceTracker.Application.Tests/Wallets/Commands/CreateWallet/CreateWalletCommandHandlerTests.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
 using Castle.Core.Logging;
+using FinanceTracker.Application.Categories.Commands.CreateCategory;
 using FinanceTracker.Application.Users;
 using FinanceTracker.Domain.Entities;
+using FinanceTracker.Domain.Exceptions;
 using FinanceTracker.Domain.Repositories;
 using FluentAssertions;
 using Microsoft.Extensions.Logging;
@@ -50,5 +52,39 @@ public class CreateWalletCommandHandlerTests
         wallet.UserId.Should().Be("test");
         walletRepositoryMock.Verify(r => r.Create(wallet), Times.Once);
       
+    }
+    [Fact()]
+    public async Task Handle_ForUserNull_ReturnsForbiddenException()
+    {
+        // Arrange
+
+        var loggerMock = new Mock<ILogger<CreateWalletCommandHandler>>();
+
+        var userContextMock = new Mock<IUserContext>();
+        userContextMock
+            .Setup(u => u.GetUser())
+            .Returns((UserDto?)null);
+
+        var mapperMock = new Mock<IMapper>();
+        var command = new CreateWalletCommand();
+        var wallet = new Wallet();
+        mapperMock
+            .Setup(m => m.Map<CreateWalletCommand, Wallet>(command))
+            .Returns(wallet);
+
+        var walletRepositoryMock = new Mock<IWalletRepository>();
+        walletRepositoryMock
+            .Setup(repo => repo.Create(It.IsAny<Wallet>()))
+            .ReturnsAsync(1);
+
+        var commandHandler = new CreateWalletCommandHandler(loggerMock.Object, userContextMock.Object, mapperMock.Object, walletRepositoryMock.Object);
+
+        // Act
+
+        await FluentActions
+        .Invoking(() => commandHandler.Handle(command, CancellationToken.None))
+        .Should()
+        .ThrowAsync<ForbiddenException>();
+
     }
 }
