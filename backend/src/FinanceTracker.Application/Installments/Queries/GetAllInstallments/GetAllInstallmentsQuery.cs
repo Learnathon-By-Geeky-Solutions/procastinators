fa22eb1e1f5d7f5c81b4fb0b2ec7d95c0164f8ae
Ashik150 <sskhan4409@gmail.com>
2025-04-19 00:56:30 +0600

    feat: GetAllInstalmentsQuery Implemented

---
+++ backend/src/FinanceTracker.Application/Installments/Queries/GetAllInstallments/GetAllInstallmentsQuery.cs
@@ -0,0 +1,9 @@
+﻿
+using FinanceTracker.Application.Installments.Dtos;
+using MediatR;
+
+namespace FinanceTracker.Application.Installments.Queries.GetAllInstallments;
+
+public class GetAllInstallmentsQuery : IRequest<IEnumerable<InstallmentDto>>
+{
+}
