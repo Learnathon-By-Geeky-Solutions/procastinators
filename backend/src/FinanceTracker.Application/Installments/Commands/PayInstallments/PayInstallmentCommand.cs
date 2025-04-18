31859eb3e4ad9674aa28a43d8dfc76827d83283e
Ashik150 <sskhan4409@gmail.com>
2025-04-19 01:48:56 +0600

    feat: PayInstallmentCommand Implemented

---
+++ backend/src/FinanceTracker.Application/Installments/Commands/PayInstallments/PayInstallmentCommand.cs
@@ -0,0 +1,12 @@
+﻿
+using MediatR;
+
+namespace FinanceTracker.Application.Installments.Commands.PayInstallments;
+
+public class PayInstallmentCommand : IRequest<int>
+{
+    public int LoanId { get; set; } = default!;
+    public decimal Amount { get; set; } = default!;
+    public string? Note { get; set; } = default!;
+    public DateTime NextDueDate { get; set; } = default!;
+}
