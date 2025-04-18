83b39fe2776ba5a13f36571ae19e48f1fbf76b4c
Ashik150 <sskhan4409@gmail.com>
2025-04-19 01:57:09 +0600

    feat: PayInstallmentCommandHandler Implemented int Application Layer

---
+++ backend/src/FinanceTracker.Application/Installments/Commands/PayInstallments/PayInstallmentCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿
+using FinanceTracker.Domain.Entities;
+using FinanceTracker.Domain.Exceptions;
+using FinanceTracker.Domain.Repositories;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace FinanceTracker.Application.Installments.Commands.PayInstallments;
+
+public class PayInstallmentCommandHandler(
+    ILoanRepository loanRepo,
+    IInstallmentRepository installmentRepo,
+    ILogger<PayInstallmentCommandHandler> logger
+) : IRequestHandler<PayInstallmentCommand, int>
+{
+    public async Task<int> Handle(PayInstallmentCommand request, CancellationToken cancellationToken)
+    {
+        var loan = await loanRepo.GetByIdAsync(request.LoanId);
+        if (loan == null || loan.IsDeleted)
+            throw new NotFoundException("Loan",request.LoanId.ToString());
+
+        // Subtract the paid amount from the loan's due amount
+        loan.DueAmount -= request.Amount;
+
+        var installment = new Installment
+        {
+            LoanId = request.LoanId,
+            Amount = request.Amount,
+            Note = request.Note,
+            NextDueDate = request.NextDueDate
+        };
+
+        await installmentRepo.CreateAsync(installment);
+        await loanRepo.SaveChangesAsync(); 
+        await installmentRepo.SaveChangesAsync();
+
+        logger.LogInformation("Installment paid: {@installment}", installment);
+        return installment.Id;
+    }
+}
