390f0991032e247bd582eb32f9640dea829760e2
Ashik150 <sskhan4409@gmail.com>
2025-04-19 00:49:46 +0600

    feat: GetInstallmentByIdQueryHandler Implemented

---
+++ backend/src/FinanceTracker.Application/Installments/Queries/GetInstallmentById/GetInstallmentByIdQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿
+using AutoMapper;
+using FinanceTracker.Application.Installments.Dtos;
+using FinanceTracker.Domain.Exceptions;
+using FinanceTracker.Domain.Repositories;
+using MediatR;
+
+namespace FinanceTracker.Application.Installments.Queries.GetInstallmentById;
+
+public class GetInstallmentByIdQueryHandler(
+    IInstallmentRepository installmentRepo,
+    IMapper mapper)
+    : IRequestHandler<GetInstallmentByIdQuery, InstallmentDto>
+{
+    public async Task<InstallmentDto> Handle(GetInstallmentByIdQuery request, CancellationToken cancellationToken)
+    {
+        var installment = await installmentRepo.GetByIdAsync(request.Id);
+        if (installment == null)
+        {
+            throw new NotFoundException("Installment", request.Id.ToString());
+        }
+
+        return mapper.Map<InstallmentDto>(installment);
+    }
+}
