b1f74cd056910924191ad8a5b75e07977d2c15a2
Ashik150 <sskhan4409@gmail.com>
2025-04-18 19:57:55 +0600

    feat: GetLoanByIdQuery implemented

---
+++ backend/src/FinanceTracker.Application/LoanRequests/Queries/GetLoanById/GetLoanByIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using FinanceTracker.Application.LoanRequests.Dtos.LoanDTO;
+using MediatR;
+
+namespace FinanceTracker.Application.LoanRequests.Queries.GetLoanById;
+
+public class GetLoanByIdQuery : IRequest<LoanDto>
+{
+    public int Id { get; set; }
+
+    public GetLoanByIdQuery(int id)
+    { 
+        Id = id;
+    }
+}
