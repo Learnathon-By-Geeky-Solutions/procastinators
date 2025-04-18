f6f2408f537563829070e0a8f4231a7898ad77f7
Ashik150 <sskhan4409@gmail.com>
2025-04-18 20:17:27 +0600

    feat: GetAllLoansQuery implemented

---
+++ backend/src/FinanceTracker.Application/LoanRequests/Queries/GetAllLoans/GetAllLoansQuery.cs
@@ -0,0 +1,14 @@
+﻿
+using FinanceTracker.Application.LoanRequests.Dtos.LoanDTO;
+using MediatR;
+
+namespace FinanceTracker.Application.LoanRequests.Queries.GetAllLoans;
+
+public class GetAllLoansQuery : IRequest<IEnumerable<LoanDto>>
+{
+    public string LenderId { get; set; }
+    public GetAllLoansQuery(string lenderId)
+    {
+        LenderId = lenderId;
+    }
+}
