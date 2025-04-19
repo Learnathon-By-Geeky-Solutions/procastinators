3e2a5482ab662aa0f3fcf533ab88d398245531c5
Ashik150 <sskhan4409@gmail.com>
2025-04-19 10:17:18 +0600

    feat: CreateLoan Controller Define

--- backend/src/FinanceTracker.Api/Controllers/LoanController.cs
+++ backend/src/FinanceTracker.Api/Controllers/LoanController.cs
@@ -1,4 +1,6 @@
-﻿using MediatR;
+﻿using FinanceTracker.Application.LoanRequests.Commands.CreateLoan;
+using FinanceTracker.Application.LoanRequests.Queries.GetLoanById;
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,5 +11,10 @@ namespace FinanceTracker.Api.Controllers;
 [Authorize]
 public class LoanController(IMediator mediator) : ControllerBase
 {
-   
+    [HttpPost]
+    public async Task<IActionResult> CreateLoan(CreateLoanCommand command)
+    {
+        var id = await mediator.Send(command);
+        return CreatedAtAction(nameof(GetLoanByIdQuery), new { id }, null);
+    }
 }
