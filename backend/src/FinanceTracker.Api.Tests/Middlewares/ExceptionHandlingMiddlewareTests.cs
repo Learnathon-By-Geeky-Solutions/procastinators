625e683956221f8488f2b17c480a1f1df557fdd3
rafio1020 <rafio.work.2002@gmail.com>
2025-04-20 02:59:00 +0600

    test: add exception handling middleware when no exception thrown should call next delegate

---
+++ backend/src/FinanceTracker.Api.Tests/Middlewares/ExceptionHandlingMiddlewareTests.cs
@@ -0,0 +1,30 @@
+﻿using Castle.Core.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FinanceTracker.Api.Middlewares.Tests;
+
+public class ExceptionHandlingMiddlewareTests
+{
+    [Fact()]
+    public async Task InvokeAsync_WhenNoExceptionThrown_ShouldCallNextDelegate()
+    {
+        // Arrange
+
+        var loggerMock = new Mock<ILogger<ExceptionHandlingMiddleware>>();
+        var middleware = new ExceptionHandlingMiddleware(loggerMock.Object);
+        var context = new DefaultHttpContext();
+        var nextDelegateMock = new Mock<RequestDelegate>();
+
+        // Act
+
+        await middleware.InvokeAsync(context, nextDelegateMock.Object);
+
+        // Assert
+
+        nextDelegateMock.Verify(next => next.Invoke(context), Times.Once);
+    }
+}
