29a7249feddab308eb4a95665f643312457a6b27
Ashik150 <sskhan4409@gmail.com>
2025-04-20 21:33:12 +0600

    feat: GetAllLoanAsBorrowerQuery Implemented

---
+++ backend/src/FinanceTracker.Application/Loans/Queries/GetAllLoansAsBorrower/GetAllLoansAsBorrowerQuery.cs
@@ -0,0 +1,8 @@
+﻿using FinanceTracker.Application.LoanRequests.Dtos.LoanRequestDTO;
+using MediatR;
+
+namespace FinanceTracker.Application.Loans.Queries.GetAllLoansAsBorrower;
+
+public class GetAllLoansAsBorrowerQuery : IRequest<IEnumerable<LoanRequestDto>>
+{
+}
