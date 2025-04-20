3b30f87837d6a101e462b971ebe9853d5cce2b82
rafio1020 <rafio.work.2002@gmail.com>
2025-04-20 12:05:05 +0600

    test: add get report on categories query validator

---
+++ backend/tests/FinanceTracker.Application.Tests/PersonalTransactions/Queries/GetReportOnCategories/GetReportOnCategoriesQueryValidatorTests.cs
@@ -0,0 +1,52 @@
+﻿using FluentValidation.TestHelper;
+using Xunit;
+
+namespace FinanceTracker.Application.PersonalTransactions.Queries.GetReportOnCategories.Tests;
+
+public class GetReportOnCategoriesQueryValidatorTests
+{
+    [Theory()]
+    [InlineData("Bank")]
+    [InlineData("Cash")]
+    [InlineData("MFS")]
+    public void Validator_ForValidCommands_ShouldNotHaveValidationErrors(string types)
+    {
+        // Arrange
+
+        var query = new GetReportOnCategoriesQuery()
+        {
+            Type = types,
+            Days = 30
+        };
+
+        var validator = new GetReportOnCategoriesQueryValidator();
+
+        // Act
+
+        var results = validator.TestValidate(query);
+
+        // Assert
+        results.ShouldNotHaveAnyValidationErrors();
+        results.ShouldNotHaveValidationErrorFor(c => c.Days);
+    }
+    [Fact()]
+    public void Validator_ForInvalidCommands_ShouldNotHaveValidationErrors()
+    {
+        // Arrange
+
+        var query = new GetReportOnCategoriesQuery()
+        {
+            Type = "Bank",
+            Days = 3550
+        };
+
+        var validator = new GetReportOnCategoriesQueryValidator();
+
+        // Act
+
+        var results = validator.TestValidate(query);
+
+        // Assert
+        results.ShouldHaveValidationErrorFor(c => c.Days);
+    }
+}
