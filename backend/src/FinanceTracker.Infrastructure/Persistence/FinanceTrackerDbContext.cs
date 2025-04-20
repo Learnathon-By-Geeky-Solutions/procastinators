33899637eba8f2823affddd1ad91392e7254edb7
Ashik150 <sskhan4409@gmail.com>
2025-04-20 23:14:27 +0600

    fix: Builder name updated

--- backend/src/FinanceTracker.Infrastructure/Persistence/FinanceTrackerDbContext.cs
+++ backend/src/FinanceTracker.Infrastructure/Persistence/FinanceTrackerDbContext.cs
@@ -15,86 +15,86 @@ internal class FinanceTrackerDbContext(DbContextOptions<FinanceTrackerDbContext>
     internal DbSet<Loan> Loans { get; set; } = default!;
     internal DbSet<Installment> Installments { get; set; } = default!;
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    protected override void OnModelCreating(ModelBuilder builder)
     {
-        base.OnModelCreating(modelBuilder);
+        base.OnModelCreating(builder);
 
-        modelBuilder.Entity<Wallet>(entity =>
+        builder.Entity<Wallet>(entity =>
         {
             entity.Property(e => e.Balance).HasColumnType("decimal(18, 2)");
         });
 
-        modelBuilder.Entity<PersonalTransaction>(entity =>
+        builder.Entity<PersonalTransaction>(entity =>
         {
             entity.Property(e => e.Amount).HasColumnType("decimal(18, 2)");
         });
 
-        modelBuilder.Entity<LoanRequest>(entity =>
+        builder.Entity<LoanRequest>(entity =>
         {
             entity.Property(e => e.Amount).HasColumnType("decimal(18, 2)");
         });
 
-        modelBuilder.Entity<Loan>(entity =>
+        builder.Entity<Loan>(entity =>
         {
             entity.Property(e => e.Amount).HasColumnType("decimal(18, 2)");
             entity.Property(e => e.DueAmount).HasColumnType("decimal(18, 2)");
         });
 
-        modelBuilder.Entity<Installment>(entity =>
+        builder.Entity<Installment>(entity =>
         {
             entity.Property(e => e.Amount).HasColumnType("decimal(18, 2)");
         });
 
-        modelBuilder
+        builder
             .Entity<PersonalTransaction>()
             .HasOne(p => p.Wallet)
             .WithMany()
             .HasForeignKey(p => p.WalletId)
             .OnDelete(DeleteBehavior.Restrict);
 
-        modelBuilder
+        builder
             .Entity<PersonalTransaction>()
             .HasOne(p => p.Category)
             .WithMany()
             .HasForeignKey(p => p.CategoryId)
             .OnDelete(DeleteBehavior.Restrict);
 
-        modelBuilder
+        builder
             .Entity<PersonalTransaction>()
             .HasOne(p => p.User)
             .WithMany()
             .HasForeignKey(p => p.UserId)
             .OnDelete(DeleteBehavior.Restrict);
 
-        modelBuilder
+        builder
             .Entity<LoanRequest>()
             .HasOne(lr => lr.Borrower)
             .WithMany()
             .HasForeignKey(lr => lr.BorrowerId)
             .OnDelete(DeleteBehavior.Restrict);
 
-        modelBuilder
+        builder
             .Entity<LoanRequest>()
             .HasOne(lr => lr.Lender)
             .WithMany()
             .HasForeignKey(lr => lr.LenderId)
             .OnDelete(DeleteBehavior.Restrict);
 
-        modelBuilder
+        builder
             .Entity<Loan>()
             .HasOne(lr => lr.Lender)
             .WithMany()
             .HasForeignKey(lr => lr.LenderId)
             .OnDelete(DeleteBehavior.Restrict);
 
-        modelBuilder
+        builder
             .Entity<Loan>()
             .HasOne(lr => lr.LoanRequest)
             .WithOne()
             .HasForeignKey<Loan>(lr => lr.LoanRequestId)
             .OnDelete(DeleteBehavior.Restrict);
 
-        modelBuilder
+        builder
             .Entity<Installment>()
             .HasOne(lr => lr.Loan)
             .WithMany()
