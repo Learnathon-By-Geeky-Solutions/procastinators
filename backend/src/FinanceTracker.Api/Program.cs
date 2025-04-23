b689b11b2a36f801f8d101365735c2da70da61d0
sakibhossain323 <sakibhossain323@gmail.com>
2025-04-23 22:15:57 +0600

    build: update configuration for azure deployment

--- backend/src/FinanceTracker.Api/Program.cs
+++ backend/src/FinanceTracker.Api/Program.cs
@@ -3,45 +3,61 @@ using FinanceTracker.Api.Middlewares;
 using FinanceTracker.Application.Extensions;
 using FinanceTracker.Domain.Entities;
 using FinanceTracker.Infrastructure.Extensions;
+using FinanceTracker.Infrastructure.Seeders;
 using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddApplication();
-builder.Services.AddInfrastructure(builder.Configuration);
-
-builder.AddPresentation();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-
-app.UseSerilogRequestLogging();
-
-app.UseMiddleware<ExceptionHandlingMiddleware>();
-
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    var builder = WebApplication.CreateBuilder(args);
 
-if (app.Environment.IsProduction())
+    // Add services to the container.
+
+    builder.Services.AddApplication();
+    builder.Services.AddInfrastructure(builder.Configuration);
+
+    builder.AddPresentation();
+
+    var app = builder.Build();
+
+    var scope = app.Services.CreateScope();
+    var seeder = scope.ServiceProvider.GetRequiredService<IFinanceTrackerSeeder>();
+    await seeder.SeedAsync();
+
+    // Configure the HTTP request pipeline.
+
+    app.UseSerilogRequestLogging();
+
+    app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    if (app.Environment.IsProduction())
+    {
+        app.UseHttpsRedirection();
+    }
+
+    app.MapGroup("api/identity").WithTags("Identity").MapIdentityApi<User>();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.UseCors();
+
+    await app.RunAsync();
+}
+catch (Exception exception)
 {
-    app.UseHttpsRedirection();
+    Log.Fatal(exception, "Failed Application Startup");
 }
-
-app.MapGroup("api/identity").WithTags("Identity").MapIdentityApi<User>();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.UseCors();
-
-await app.RunAsync();
+finally
+{
+    Log.CloseAndFlush();
+}
 
 public partial class Program
 {
---
+++ backend/src/FinanceTracker.Infrastructure/Seeders/FinanceTrackerSeeder.cs
@@ -0,0 +1,15 @@
+﻿using FinanceTracker.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceTracker.Infrastructure.Seeders;
+
+internal class FinanceTrackerSeeder(FinanceTrackerDbContext dbContext) : IFinanceTrackerSeeder
+{
+    public async Task SeedAsync()
+    {
+        if (dbContext.Database.GetPendingMigrations().Any())
+        {
+            await dbContext.Database.MigrateAsync();
+        }
+    }
+}
