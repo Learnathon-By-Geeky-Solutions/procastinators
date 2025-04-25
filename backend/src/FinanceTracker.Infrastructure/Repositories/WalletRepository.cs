4e79149c59b4669da4bad4f784c1bf72caa368b6
Ashik150 <sskhan4409@gmail.com>
2025-04-25 14:25:18 +0600

    feat: Wallet Updatebalance repository done

--- backend/src/FinanceTracker.Infrastructure/Repositories/WalletRepository.cs
+++ backend/src/FinanceTracker.Infrastructure/Repositories/WalletRepository.cs
@@ -16,19 +16,30 @@ internal class WalletRepository(FinanceTrackerDbContext dbContext) : IWalletRepo
 
     public async Task<IEnumerable<Wallet>> GetAll(string userId)
     {
-        return await dbContext.Wallets
-            .Where(w => w.UserId == userId && !w.IsDeleted)
-            .ToListAsync();
+        return await dbContext.Wallets.Where(w => w.UserId == userId && !w.IsDeleted).ToListAsync();
     }
 
     public async Task<Wallet?> GetById(int id)
     {
-        return await dbContext.Wallets
-            .FirstOrDefaultAsync(w => w.Id == id);
+        return await dbContext.Wallets.FirstOrDefaultAsync(w => w.Id == id);
     }
 
     public async Task<int> SaveChangesAsync()
     {
+        return await dbContext.SaveChangesAsync();
+    }
+
+    public async Task<int> UpdateBalance(Wallet wallet)
+    {
+        var existingWallet = await dbContext.Wallets.FirstOrDefaultAsync(w =>
+            w.Id == wallet.Id && !w.IsDeleted
+        );
+        if (existingWallet != null)
+        {
+            throw new Exception("Wallet Not Found");
+        }
+
+        existingWallet!.Balance = wallet.Balance;
         return await dbContext.SaveChangesAsync();
     }
 }
