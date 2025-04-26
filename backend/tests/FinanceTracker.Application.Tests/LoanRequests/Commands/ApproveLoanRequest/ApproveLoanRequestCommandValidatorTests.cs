d6c50dda1ab6df2380b71f77f270ed005c74f764
rafio1020 <rafio.work.2002@gmail.com>
2025-04-27 00:37:32 +0600

    test: add approveloanrequestvalidator

---
+++ backend/tests/FinanceTracker.Application.Tests/LoanRequests/Commands/ApproveLoanRequest/ApproveLoanRequestCommandValidatorTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FinanceTracker.Application.LoanRequests.Commands.ApproveLoanRequest;
+using FluentValidation.TestHelper;
+using Xunit;
+
+namespace FinanceTracker.Application.LoanRequests.Commands.ApproveLoanRequest.Tests
+{
+    public class ApproveLoanRequestCommandValidatorTests
+    {
+        [Fact()]
+        public void Validator_ForValidCommand_ShouldNotHaveAnyValidationErrors()
+        {
+            // Arrange
+            var command = new ApproveLoanRequestCommand(1, 2);
+
+            var validator = new ApproveLoanRequestCommandValidator();
+
+            // Act
+            var results = validator.TestValidate(command);
+
+            // Assert
+            results.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Fact()]
+        public void Validator_ForEmptyCommand_ShouldHaveValidationErrors()
+        {
+            // Arrange
+            var command = new ApproveLoanRequestCommand(0, 0);
+
+            var validator = new ApproveLoanRequestCommandValidator();
+
+            // Act
+            var results = validator.TestValidate(command);
+
+            // Assert
+            results.ShouldHaveValidationErrorFor(c => c.LoanRequestId);
+            results.ShouldHaveValidationErrorFor(c => c.LenderWalletId);
+        }
+    }
+}
