8efa89ff7220af6dbe3620df88e1fdc82125f9de
Ashik150 <sskhan4409@gmail.com>
2025-04-26 21:44:38 +0600

    feat: approveloanrequest validator added

---
+++ backend/src/FinanceTracker.Application/LoanRequests/Commands/ApproveLoanRequest/ApproveLoanRequestCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace FinanceTracker.Application.LoanRequests.Commands.ApproveLoanRequest;
+
+public class ApproveLoanRequestCommandValidator : AbstractValidator<ApproveLoanRequestCommand>
+{
+    public ApproveLoanRequestCommandValidator()
+    {
+        RuleFor(x => x.LoanRequestId).NotEmpty().WithMessage("LoanRequestId must not be empty.");
+
+        RuleFor(x => x.LenderWalletId).NotEmpty().WithMessage("LenderWalletId must not be empty.");
+    }
+}
