bae95c58b7a7edfeea91402461569b818358956d
Ashik150 <sskhan4409@gmail.com>
2025-04-26 21:36:41 +0600

    feat: createloan validator added

--- backend/src/FinanceTracker.Application/Loans/Commands/CreateLoan/CreateLoanCommandValidator.cs
+++ backend/src/FinanceTracker.Application/Loans/Commands/CreateLoan/CreateLoanCommandValidator.cs
@@ -1,3 +1,17 @@
-﻿namespace FinanceTracker.Application.Loans.Commands.CreateLoan;
+﻿using FluentValidation;
 
-public class CreateLoanCommandValidator { }
+namespace FinanceTracker.Application.Loans.Commands.CreateLoan;
+
+public class CreateLoanCommandValidator : AbstractValidator<CreateLoanCommand>
+{
+    public CreateLoanCommandValidator()
+    {
+        RuleFor(x => x.Amount).GreaterThan(0).WithMessage("Loan amount must be greater than zero.");
+
+        RuleFor(x => x.WalletId).NotEmpty().WithMessage("WalletId must not be empty.");
+
+        RuleFor(x => x.DueDate)
+            .GreaterThan(DateTime.UtcNow)
+            .WithMessage("Due date must be in the future.");
+    }
+}
