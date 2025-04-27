908e76129848457f510d969f72bf2b873ab29e40
rafio1020 <rafio.work.2002@gmail.com>
2025-04-27 19:46:29 +0600

    test: add getinstallmentbyid and get allloansasborrower query handler

---
+++ backend/tests/FinanceTracker.Application.Tests/Installments/Queries/GetInstallmentById/GetInstallmentByIdQueryHandlerTests.cs
@@ -0,0 +1,69 @@
+﻿using AutoMapper;
+using FinanceTracker.Application.Categories.Dtos;
+using FinanceTracker.Application.Installments.Dtos;
+using FinanceTracker.Application.Installments.Queries.GetAllInstallments;
+using FinanceTracker.Domain.Entities;
+using FinanceTracker.Domain.Repositories;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace FinanceTracker.Application.Installments.Queries.GetInstallmentById.Tests;
+
+public class GetInstallmentByIdQueryHandlerTests
+{
+    private readonly Mock<IInstallmentRepository> _installmentRepositoryMock;
+    private readonly Mock<IMapper> _mapperMock;
+    private readonly GetInstallmentByIdQueryHandler _handler;
+    private readonly int _loanId = 1;
+    private readonly int _installmentId = 1;
+
+    public GetInstallmentByIdQueryHandlerTests()
+    {
+        _mapperMock = new Mock<IMapper>();
+        _installmentRepositoryMock = new Mock<IInstallmentRepository>();
+
+        _handler = new GetInstallmentByIdQueryHandler(
+            _installmentRepositoryMock.Object,
+            _mapperMock.Object
+        );
+    }
+
+    [Fact]
+    public async Task Handle_WithValidRequest_ShouldReturnAllInstallmentsForCurrentUser()
+    {
+        // Arrange
+        var query = new GetInstallmentByIdQuery(_installmentId);
+
+        var installment = new Installment
+        {
+            Id = _installmentId,
+            Amount = 100,
+            Timestamp = DateTime.Now,
+            LoanId = _loanId,
+            NextDueDate = DateTime.Now.AddDays(30),
+        };
+
+        var expectedDto = new InstallmentDto()
+        {
+            Id = _installmentId,
+            Amount = 100,
+            Timestamp = DateTime.Now,
+            LoanId = _loanId,
+            NextDueDate = DateTime.Now.AddDays(30),
+        };
+
+        _installmentRepositoryMock
+            .Setup(repo => repo.GetByIdAsync(_installmentId))
+            .ReturnsAsync(installment);
+
+        _mapperMock.Setup(m => m.Map<InstallmentDto>(installment)).Returns(expectedDto);
+
+        // Act
+        var result = await _handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.Should().Be(expectedDto);
+        _mapperMock.Verify(m => m.Map<InstallmentDto>(installment), Times.Once);
+    }
+}
