323b9b1bc928f4c3ae6d084a8286e4af7a5d9b5a
rafio1020 <rafio.work.2002@gmail.com>
2025-04-27 17:40:16 +0600

    test: add loanrequestprofile where createloanrequestcommand to loanrequest and loanrequest to loanrequestdto is mapperd correctly

---
+++ backend/tests/FinanceTracker.Application.Tests/LoanRequests/Dtos/LoanRequestDTO/LoanRequestProfileTests.cs
@@ -0,0 +1,75 @@
+﻿using AutoMapper;
+using FinanceTracker.Application.LoanRequests.Commands.CreateLoanRequest;
+using FinanceTracker.Domain.Entities;
+using FluentAssertions;
+using Xunit;
+
+namespace FinanceTracker.Application.LoanRequests.Dtos.LoanRequestDTO.Tests;
+
+public class LoanRequestProfileTests
+{
+    private IMapper _mapper;
+
+    public LoanRequestProfileTests()
+    {
+        var configuration = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<LoanRequestProfile>();
+        });
+        _mapper = configuration.CreateMapper();
+    }
+
+    [Fact()]
+    public void CreateMap_ForCreateLoanRequestCommandToLoanRequest_MapsCorrectly()
+    {
+        // Arrange
+        var command = new CreateLoanRequestCommand()
+        {
+            Amount = 1000,
+            Note = "Test Loan Request",
+            DueDate = DateTime.UtcNow.AddDays(30),
+            LenderId = "1",
+            WalletId = 2,
+        };
+
+        // Act
+        var loanRequest = _mapper.Map<Domain.Entities.LoanRequest>(command);
+
+        // Assert
+        loanRequest.Should().NotBeNull();
+        loanRequest.Amount.Should().Be(command.Amount);
+        loanRequest.Note.Should().Be(command.Note);
+        loanRequest.DueDate.Should().Be(command.DueDate);
+        loanRequest.LenderId.Should().Be(command.LenderId);
+        loanRequest.WalletId.Should().Be(command.WalletId);
+    }
+
+    [Fact()]
+    public void CreateMap_ForLoanRequestToLoanRequestDto_MapsCorrectly()
+    {
+        // Arrange
+        var loanRequest = new LoanRequest()
+        {
+            Id = 1,
+            Amount = 1000,
+            Note = "Test Loan Request",
+            DueDate = DateTime.UtcNow.AddDays(30),
+            LenderId = "2",
+            BorrowerId = "3",
+            IsApproved = false,
+        };
+
+        // Act
+        var loanRequestDto = _mapper.Map<LoanRequestDto>(loanRequest);
+
+        // Assert
+        loanRequestDto.Should().NotBeNull();
+        loanRequestDto.Id.Should().Be(loanRequest.Id);
+        loanRequestDto.Amount.Should().Be(loanRequest.Amount);
+        loanRequestDto.Note.Should().Be(loanRequest.Note);
+        loanRequestDto.DueDate.Should().Be(loanRequest.DueDate);
+        loanRequestDto.LenderId.Should().Be(loanRequest.LenderId);
+        loanRequestDto.BorrowerId.Should().Be(loanRequest.BorrowerId);
+        loanRequestDto.IsApproved.Should().Be(loanRequest.IsApproved);
+    }
+}
