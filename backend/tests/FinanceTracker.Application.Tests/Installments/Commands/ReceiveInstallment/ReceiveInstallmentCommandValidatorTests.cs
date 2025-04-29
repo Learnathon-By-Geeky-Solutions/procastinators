cd55432c60a6487f51e13d337c763cfd1fe17f66
rafio1020 <rafio.work.2002@gmail.com>
2025-04-29 13:27:16 +0600

    test: add some for commands which returns validation error in receiveonstallmentvalidator

--- backend/tests/FinanceTracker.Application.Tests/Installments/Commands/ReceiveInstallment/ReceiveInstallmentCommandValidatorTests.cs
+++ backend/tests/FinanceTracker.Application.Tests/Installments/Commands/ReceiveInstallment/ReceiveInstallmentCommandValidatorTests.cs
@@ -20,5 +20,30 @@ public class ReceiveInstallmentCommandValidatorTests
 
         // Act
         var results = validator.TestValidate(command);
+
+        // Assert
+        results.ShouldNotHaveAnyValidationErrors();
+    }
+
+    [Fact()]
+    public void Validator_ForInvalidCommand_ShouldNotHaveValidationErrors()
+    {
+        // Arrange
+        var command = new ReceiveInstallmentCommand
+        {
+            WalletId = 0,
+            Amount = -100,
+            NextDueDate = DateTime.UtcNow.AddDays(-1),
+        };
+
+        var validator = new ReceiveInstallmentCommandValidator();
+
+        // Act
+        var results = validator.TestValidate(command);
+
+        // Assert
+        results.ShouldHaveValidationErrorFor(c => c.WalletId);
+        results.ShouldHaveValidationErrorFor(c => c.Amount);
+        results.ShouldHaveValidationErrorFor(c => c.NextDueDate);
     }
 }
