81a2a6b6f712016ae20a4767567391817c3bbcb6
rafio1020 <rafio.work.2002@gmail.com>
2025-04-29 13:24:32 +0600

    test: add recieveinstallmentsvalidator, fix: receiveinstallmentsvalidator abstract validator changed to receiveinstallmentcommand

---
+++ backend/tests/FinanceTracker.Application.Tests/InstallmentClaims/Commands/ClaimInstallmentFund/ClaimInstallmentFundCommandHandlerTests.cs
@@ -0,0 +1,9 @@
+﻿using Xunit;
+
+namespace FinanceTracker.Application.InstallmentClaims.Commands.ClaimInstallmentFund.Tests;
+
+public class ClaimInstallmentFundCommandHandlerTests
+{
+    [Fact()]
+    public void HandleTest() { }
+}
---
+++ backend/tests/FinanceTracker.Application.Tests/Installments/Commands/ReceiveInstallment/ReceiveInstallmentCommandValidatorTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation.TestHelper;
+using Xunit;
+
+namespace FinanceTracker.Application.Installments.Commands.ReceiveInstallment.Tests;
+
+public class ReceiveInstallmentCommandValidatorTests
+{
+    [Fact()]
+    public void Validator_ForValidCommand_ShouldNotHaveValidationErrors()
+    {
+        // Arrange
+        var command = new ReceiveInstallmentCommand
+        {
+            WalletId = 2,
+            Amount = 100,
+            NextDueDate = DateTime.UtcNow.AddDays(30),
+        };
+
+        var validator = new ReceiveInstallmentCommandValidator();
+
+        // Act
+        var results = validator.TestValidate(command);
+    }
+}
